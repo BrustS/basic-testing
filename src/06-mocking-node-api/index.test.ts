b30d7b48b59cf72012944341df2359306ffd5f01
BrustS <xbrust88x@gmail.com>
2024-10-20 15:33:03 +0300

    fix: solution all test

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,4 +1,6 @@
 import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import path from 'path';
+import fs from 'fs';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -9,12 +11,23 @@ describe('doStuffByTimeout', () => {
     jest.useRealTimers();
   });
 
+  beforeEach(() => {
+    jest.spyOn(global, 'setTimeout');
+  });
+
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByTimeout(callback, 3000);
+    expect(setTimeout).toHaveBeenCalledWith(callback, 3000);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByTimeout(callback, 3000);
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(3000);
+    expect(callback).toBeCalledTimes(1);
   });
 });
 
@@ -28,24 +41,51 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByInterval(callback, 2000);
+    expect(callback).not.toBeCalled();
+    jest.advanceTimersByTime(2000);
+    expect(callback).toBeCalledTimes(1);
+    jest.advanceTimersByTime(2000);
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByInterval(callback, 1000);
+    jest.advanceTimersByTime(4000);
+    expect(callback).toHaveBeenCalledTimes(4);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    const fileName = 'test.txt';
+    const spy = jest.spyOn(path, 'join');
+    await readFileAsynchronously(fileName);
+    expect(spy).toHaveBeenCalledWith(__dirname, fileName);
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    const pathToFile = 'text.txt';
+    jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+    const read = jest.spyOn(fs.promises, 'readFile');
+    const result = await readFileAsynchronously(pathToFile);
+    expect(read).not.toHaveBeenCalled();
+    expect(result).toBe(null);
+    jest.restoreAllMocks();
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    const pathToFile = 'text.txt';
+    const content = 'text';
+    jest.spyOn(fs, 'existsSync').mockReturnValue(true);
+    const read = jest
+      .spyOn(fs.promises, 'readFile')
+      .mockReturnValue(Promise.resolve(content));
+    const result = await readFileAsynchronously(pathToFile);
+    expect(read).toHaveBeenCalled();
+    expect(result).toBe(content);
+    jest.restoreAllMocks();
   });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,38 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const checkList = [12, 23, 34, 45, 56, 67];
+    const expectedLinkedList = {
+      value: 12,
+      next: {
+        value: 23,
+        next: {
+          value: 34,
+          next: {
+            value: 45,
+            next: {
+              value: 56,
+              next: {
+                value: 67,
+                next: {
+                  value: null,
+                  next: null,
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    expect(generateLinkedList(checkList)).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [12, 23, 34];
+    const list = generateLinkedList(elements);
+    expect(list).toMatchSnapshot();
   });
 });
